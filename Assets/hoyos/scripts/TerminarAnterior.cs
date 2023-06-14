794171fa984f9393720e95f066c5b472eca17677
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-06-14 03:03:14 +0200

    Terminar anterior
    Mism Dinamica que candados pero para impedir retroceder

---
+++ Assets/hoyos/scripts/TerminarAnterior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TerminarAnterior : MonoBehaviour
+{
+    //en este script habrá un metodo que se llame al pulsar 1 vez en cada boton que hará 2 cosas
+    //cambiar el sprite del anterior boton a un doble tick
+    //hacer el anterior boton no interactuable una vez se pulsa el nuevo
+
+    [SerializeField]
+    private Sprite dobleTick;
+
+    [SerializeField]
+    private Button botonAnteriorExcavacion;
+
+    public void CerrarExcavacion()
+    {
+        //cambiamos a sprite Pico
+        botonAnteriorExcavacion.GetComponent<Image>().sprite = dobleTick;
+        //hacemos boton interactuable
+        botonAnteriorExcavacion.GetComponent<Button>().interactable = false;
+    }
+}
