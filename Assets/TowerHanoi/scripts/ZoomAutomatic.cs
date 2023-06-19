0483536db58a6536c080cf5f17247828ed400cca
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-06-19 16:26:22 +0200

    zoom boton hanoi

---
+++ Assets/TowerHanoi/scripts/ZoomAutomatic.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ZoomAutomatic : MonoBehaviour
+{
+    public bool ZoomActive;
+
+    public Vector3[] target;
+
+    public Camera cam;
+
+    public float speed;
+
+    private void Start()
+    {
+
+        cam = Camera.main;
+    }
+
+    public void ZoomIn()
+    {
+        ZoomActive = true;
+    }
+
+    public void ZoomOut()
+    {
+        ZoomActive = false;
+    }
+
+
+    public void LateUpdate()
+    {
+        if(ZoomActive)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 1.2f, speed);
+        }
+        else
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 3f, speed);
+        }
+    }
+
+
+}
