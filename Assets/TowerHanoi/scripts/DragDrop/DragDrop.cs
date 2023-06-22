a70fda1a624ff3c2b34734e0add51fbfecea12bd
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-06-22 17:06:16 +0200

    itemSlot

--- Assets/TowerHanoi/scripts/DragDrop/DragDrop.cs
+++ Assets/TowerHanoi/scripts/DragDrop/DragDrop.cs
@@ -3,12 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler
+public class DragDrop : MonoBehaviour, IPointerDownHandler,IBeginDragHandler,IEndDragHandler, IDragHandler, IDropHandler
 {
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+
+    //BEGIN DRAG
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+    //hacer drag
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    //END DRAG
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
     //se llamará a esta funcion cuando se apriete el ratón
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
+    }
+
+    //hacer drop
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
     }
 }
 
