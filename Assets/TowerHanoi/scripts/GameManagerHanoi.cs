fca682798704d6b4b6e72cc1624c9776626c4997
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-06-22 22:21:25 +0200

    Update GameManagerHanoi.cs

--- Assets/TowerHanoi/scripts/GameManagerHanoi.cs
+++ Assets/TowerHanoi/scripts/GameManagerHanoi.cs
@@ -6,13 +6,22 @@ public class GameManagerHanoi : MonoBehaviour
 {
 
     //singleton
+    static private GameManagerHanoi _instanceHanoi;
+
     private void Awake()
     {
-        
+        //si la instancia no existe se hace este script la instancia
+        if (_instanceHanoi == null)
+        {
+            _instanceHanoi = this;
+        }
+        //si la instancia existe , destruimos la copia
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
-
-
-
+  
     // Start is called before the first frame update
     void Start()
     {
