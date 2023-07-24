108edb17cb9e48f34566be0a7b6cb116673c944a
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-07-24 12:54:28 +0200

    firebase data base

--- Assets/PruebaFirebase/DataBaseManager/DataBaseManager.cs
+++ Assets/PruebaFirebase/DataBaseManager/DataBaseManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Database;
+using Firebase.Firestore;
+using Firebase.Extensions;
 using UnityEngine.UI;
 using TMPro;
 using System;
@@ -19,11 +21,122 @@ public class DataBaseManager : MonoBehaviour
     private string userID;
     private string CustomUserID;
     private DatabaseReference dbReference;
+    #region tutorial
+        private FirebaseApp _app;
+    #endregion
+
+
     //Start is called before the first frame update
     void Start()
     {
         userID = SystemInfo.deviceUniqueIdentifier;
         dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        #region ServiciosGooglePlay
+
+            Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+                var dependencyStatus = task.Result;
+                if (dependencyStatus == Firebase.DependencyStatus.Available)
+                {
+                    // Create and hold a reference to your FirebaseApp,
+                    // where app is a Firebase.FirebaseApp property of your application class.
+                    _app = Firebase.FirebaseApp.DefaultInstance;
+
+                    // Set a flag here to indicate whether Firebase is ready to use by your app.
+
+
+                    //si todo ha funcionado bien decimos que llame a login
+                    LoginTutorial();
+                }
+                else
+                {
+                    UnityEngine.Debug.LogError(System.String.Format(
+                      "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                    // Firebase Unity SDK is not safe to use here.
+                }
+            });
+
+        #endregion
+    }
+
+    private void AddDataFirestore()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(auth.CurrentUser.UserId);
+        Dictionary<string, object> user = new Dictionary<string, object>
+        {
+                { "First", "QQQ" },
+                { "Last", "Lovelace" },
+                { "Born", 1815 },
+        };
+        docRef.UpdateAsync(user).ContinueWithOnMainThread(task => {
+            Debug.Log("Added data to the alovelace document in the users collection.");
+
+            GetDataFirestore();
+        });
+
+       
+    }
+
+    private void GetDataFirestore()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        //cogemos la coleccion users y tenemos unareferencia a esta usersRef
+        CollectionReference usersRef = db.Collection("users");
+        //coge como esté la base de datos ahora mismo y te la descargas
+        usersRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            //recorremos nuestro documento
+            QuerySnapshot snapshot = task.Result;
+            foreach (DocumentSnapshot document in snapshot.Documents)
+            {
+                Debug.Log(message:$"User: {document.Id}");
+                Dictionary<string, object> documentDictionary = document.ToDictionary();
+                Debug.Log(message: $"First: {documentDictionary["First"]}");
+                if (documentDictionary.ContainsKey("Middle"))
+                {
+                    Debug.Log(message: $"Middle: {documentDictionary["Middle"]}");
+                }
+
+                Debug.Log(message: $"Last: { documentDictionary["Last"]}");
+                Debug.Log(message: $"Born: {documentDictionary["Born"]}");
+            }
+
+            Debug.Log("Read all data from the users collection.");
+        });
+    }
+
+    private void LoginTutorial()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        //si el usuario se ha identificado antes currentUser poseerá sus datos
+        if(auth.CurrentUser!=null)
+        {
+            Debug.Log("Ya estaba autentificado");
+            //se conecta con base de datos firebase local data(no realtime)
+            AddDataFirestore();
+            return;
+        }
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+
+            //se conecta con base de datos firebase local data(no realtime)
+            AddDataFirestore();
+        });
     }
 
     //creamos nuevo usuario en base de datos
--- Assets/hoyos/scripts/DataBaseManager/DataBaseManagerHoyos.cs
+++ Assets/hoyos/scripts/DataBaseManager/DataBaseManagerHoyos.cs
@@ -6,6 +6,9 @@ using Firebase.Database;
 using UnityEngine.UI;
 using TMPro;
 using System;
+
+using Firebase.Firestore;
+using Firebase.Extensions;
 public class DataBaseManagerHoyos : MonoBehaviour
 {
     public InputField NamePlayer;
@@ -35,78 +38,81 @@ public class DataBaseManagerHoyos : MonoBehaviour
        _myGameManager = GameManager.GetInstance();
     }
 
-    //creamos nuevo usuario en base de datos
-    public void CreateUserInicial()
-    {
-        //Habiendo puesto al principio el nombre y el correo electronico estará guardado en las 2 variables InputField
-        //que se podrán acceder desde un método
-        //despues debemos registrar con llamada a GameManager con otro método el time y el numero de ticks totales
 
-        //registramos los 2 valores que es nombre y correo en el objeto clase user
-        //los otros 2 valores de 0 por ahora se quedan así
-        int[] valorInicialNumeroPicadas = new int[0];
-        HoyoInfo newUser = new HoyoInfo(NamePlayer.text, MailPlayer.text,0,0, valorInicialNumeroPicadas);
-        //guardamos info del objeto como un string
-        string json = JsonUtility.ToJson(newUser);
-        //escribimos en carpeta users ese string con la info guardada
-        dbReference.Child("pruebaHoyos").Child(userID).SetRawJsonValueAsync(json);
-    }
+    #region realTimeDatabase
 
-    //metodo que se llama cuando se acaba la prueba y que actualiza los datos del usuario
-    public void CreateUserTotal()
-    {
-        //declaramos tamaño array hoyos
-        numpicadasHoyosIndiv = new int[6];
-        //Habiendo puesto al principio el nombre y el correo electronico estará guardado en las 2 variables InputField
-        //que se podrán acceder desde un método
-        //despues debemos registrar con llamada a GameManager con otro método el time y el numero de ticks totales
-        int totalTime = _myGameManager.NumSecsPartidaReturn();
-        int numExcavacionesTotales = _myGameManager.NumExcavacionesTotales();
-        //numero picadas totales cada hoyo
-        numpicadasHoyosIndiv = _myGameManager.DevolverPicadasHoyo();
+            //creamos nuevo usuario en base de datos
+            public void CreateUserInicial()
+            {
+                //Habiendo puesto al principio el nombre y el correo electronico estará guardado en las 2 variables InputField
+                //que se podrán acceder desde un método
+                //despues debemos registrar con llamada a GameManager con otro método el time y el numero de ticks totales
 
-        //registramos los 2 valores que es nombre y correo en el objeto clase user
-        //los otros 2 valores de 0 por ahora se quedan así
-        HoyoInfo newUser = new HoyoInfo(NamePlayer.text, MailPlayer.text, numExcavacionesTotales, totalTime,numpicadasHoyosIndiv);
-        //guardamos info del objeto como un string
-        string json = JsonUtility.ToJson(newUser);
-        //escribimos en carpeta users ese string con la info guardada
-        dbReference.Child("pruebaHoyos").Child(userID).SetRawJsonValueAsync(json);
-    }
+                //registramos los 2 valores que es nombre y correo en el objeto clase user
+                //los otros 2 valores de 0 por ahora se quedan así
+                int[] valorInicialNumeroPicadas = new int[0];
+                HoyoInfo newUser = new HoyoInfo(NamePlayer.text, MailPlayer.text,0,0, valorInicialNumeroPicadas);
+                //guardamos info del objeto como un string
+                string json = JsonUtility.ToJson(newUser);
+                //escribimos en carpeta users ese string con la info guardada
+                dbReference.Child("pruebaHoyos").Child(userID).SetRawJsonValueAsync(json);
+            }
+
+            //metodo que se llama cuando se acaba la prueba y que actualiza los datos del usuario
+            public void CreateUserTotal()
+            {
+                //declaramos tamaño array hoyos
+                numpicadasHoyosIndiv = new int[6];
+                //Habiendo puesto al principio el nombre y el correo electronico estará guardado en las 2 variables InputField
+                //que se podrán acceder desde un método
+                //despues debemos registrar con llamada a GameManager con otro método el time y el numero de ticks totales
+                int totalTime = _myGameManager.NumSecsPartidaReturn();
+                int numExcavacionesTotales = _myGameManager.NumExcavacionesTotales();
+                //numero picadas totales cada hoyo
+                numpicadasHoyosIndiv = _myGameManager.DevolverPicadasHoyo();
+
+                //registramos los 2 valores que es nombre y correo en el objeto clase user
+                //los otros 2 valores de 0 por ahora se quedan así
+                HoyoInfo newUser = new HoyoInfo(NamePlayer.text, MailPlayer.text, numExcavacionesTotales, totalTime,numpicadasHoyosIndiv);
+                //guardamos info del objeto como un string
+                string json = JsonUtility.ToJson(newUser);
+                //escribimos en carpeta users ese string con la info guardada
+                dbReference.Child("pruebaHoyos").Child(userID).SetRawJsonValueAsync(json);
+            }
 
 
 
-    //obtener nombre de carpeta users, valor de namePlayer
-    public IEnumerator GetNamePlayer(Action<string> onCallback)
-    {
-        //hacemos carpeta Users
-        var userNamePlayerData = dbReference.Child("pruebaHoyos").Child(userID).Child("namePlayer").GetValueAsync();
+            //obtener nombre de carpeta users, valor de namePlayer
+            public IEnumerator GetNamePlayer(Action<string> onCallback)
+            {
+                //hacemos carpeta Users
+                var userNamePlayerData = dbReference.Child("pruebaHoyos").Child(userID).Child("namePlayer").GetValueAsync();
 
-        yield return new WaitUntil(predicate: () => userNamePlayerData.IsCompleted);
+                yield return new WaitUntil(predicate: () => userNamePlayerData.IsCompleted);
 
-        if (userNamePlayerData != null)
-        {
-            DataSnapshot snapshot = userNamePlayerData.Result;
+                if (userNamePlayerData != null)
+                {
+                    DataSnapshot snapshot = userNamePlayerData.Result;
 
-            onCallback.Invoke(snapshot.Value.ToString());
-        }
-    }
+                    onCallback.Invoke(snapshot.Value.ToString());
+                }
+            }
 
-    //obtener numero de carpeta users, valor de mailPlayer
-    public IEnumerator GetMailPlayer(Action<string> onCallback)
-    {
-        //hacemos carpeta Users
-        var userMailPlayerData = dbReference.Child("pruebaHoyos").Child(userID).Child("mailPlayer").GetValueAsync();
+            //obtener numero de carpeta users, valor de mailPlayer
+            public IEnumerator GetMailPlayer(Action<string> onCallback)
+            {
+                //hacemos carpeta Users
+                var userMailPlayerData = dbReference.Child("pruebaHoyos").Child(userID).Child("mailPlayer").GetValueAsync();
 
-        yield return new WaitUntil(predicate: () => userMailPlayerData.IsCompleted);
+                yield return new WaitUntil(predicate: () => userMailPlayerData.IsCompleted);
 
-        if (userMailPlayerData != null)
-        {
-            DataSnapshot snapshot = userMailPlayerData.Result;
+                if (userMailPlayerData != null)
+                {
+                    DataSnapshot snapshot = userMailPlayerData.Result;
 
-            onCallback.Invoke(snapshot.Value.ToString());
-        }
-    }
+                    onCallback.Invoke(snapshot.Value.ToString());
+                }
+            }
 
     ////coge datos de la base de datos namePlayer y mailPlayer, su resultado y lo escribe
     //public void GetUserInfo()
@@ -135,5 +141,12 @@ public class DataBaseManagerHoyos : MonoBehaviour
     //{
     //    dbReference.Child("users").Child(userID).Child("mailPlayer").SetValueAsync(MailPlayerText.text);
     //}
+
+    #endregion
+
+
+    #region dataBase
+
+    #endregion
 
 }
