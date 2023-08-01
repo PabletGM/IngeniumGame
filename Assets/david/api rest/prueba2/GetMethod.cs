dfeb627228704b66d4c79238b033d98f2b47318f
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-08-01 20:20:43 +0200

    GetApiRest david

---
+++ Assets/david/api rest/prueba2/GetMethod.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GetMethod : MonoBehaviour
+{
+    InputField outputArea;
+
+    private string json_;
+
+    
+
+    void Start()
+    {
+        outputArea = GameObject.Find("OutputArea").GetComponent<InputField>(); // Agregamos el paréntesis de cierre aquí
+        GameObject.Find("GetButton").GetComponent<Button>().onClick.AddListener(GetData);
+    }
+
+    public void GetData() => StartCoroutine(GetData_Coroutine());
+
+    IEnumerator GetData_Coroutine()
+    {
+        outputArea.text = "Loading...";
+        string uri = "https://eu-west-1.aws.data.mongodb-api.com/app/ingenuity-application-lfzzr/endpoint/Ingenuity/Test";
+        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        {
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                outputArea.text = request.error;
+            }
+            else
+            {
+                outputArea.text = request.downloadHandler.text;
+                //Debug.Log(request.downloadHandler.text);
+                json_ = request.downloadHandler.text;
+            }
+        }
+    }
+
+
+
+    public void VerJSON()
+    {
+        Debug.Log(json_);
+    }
+}
