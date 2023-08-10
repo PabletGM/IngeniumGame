3c5901cd0b6bc9c127dedd9dec9d5f40a067dc44
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-08-10 18:31:33 +0200

    conexion capacidadAdaptacion1y2

--- Assets/tests/grupo1tests/capacidad/scripts/CapacidadAdaptacionManager.cs
+++ Assets/tests/grupo1tests/capacidad/scripts/CapacidadAdaptacionManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class CapacidadAdaptacionManager : MonoBehaviour
 {
-    #region panelesJerarquia
+    #region panelesJerarquiaCapacidad1
     [SerializeField]
     private GameObject menuPrincipal;
 
@@ -29,15 +29,49 @@ public class CapacidadAdaptacionManager : MonoBehaviour
 
     #endregion
 
-    #region resultadoPrueba
+    #region resultadoPruebaCapacidad1
     private string resultadoPruebaCapacidad1 = "";
+    private int resultadoNumPruebaCapacidad1 = 0;
     #endregion
 
+    #region panelesJerarquiaCapacidad2
+    [SerializeField]
+    private GameObject menuPrincipal2;
 
-    private void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject panelesABC2;
+
+    [SerializeField]
+    private GameObject PanelA2;
+
+    [SerializeField]
+    private GameObject PanelB2;
+
+    [SerializeField]
+    private GameObject PanelC2;
+
+    [SerializeField]
+    private GameObject botonContinue2;
+
+    [SerializeField]
+    private TMP_Text opcionElegidaResult2;
+
+    #endregion
+
+    #region resultadoPruebaCapacidad1
+    private string resultadoPruebaCapacidad2 = "";
+    private int resultadoNumPruebaCapacidad2 = 0;
+    #endregion
+
+    [SerializeField]
+    private GameObject test1Capacidad;
+
+    [SerializeField]
+    private GameObject test2Capacidad;
+
+
+
+    #region methods Capacidad1
 
     //metodo opcion A que abre panel opcion A de la pregunta 1
     public void OpenOptionA1()
@@ -95,6 +129,9 @@ public class CapacidadAdaptacionManager : MonoBehaviour
         resultadoPruebaCapacidad1 = "a";
         opcionElegidaResult1.text = resultadoPruebaCapacidad1;
 
+        //ponemos valor numerico segun el resultado
+        resultadoNumPruebaCapacidad1 = 1;
+
     }
 
     //metodo que elige la opcion B de la prueba1 de capacidad
@@ -102,6 +139,9 @@ public class CapacidadAdaptacionManager : MonoBehaviour
     {
         resultadoPruebaCapacidad1 = "b";
         opcionElegidaResult1.text = resultadoPruebaCapacidad1;
+
+        //ponemos valor numerico segun el resultado
+        resultadoNumPruebaCapacidad1 = 2;
     }
 
     //metodo que elige la opcion C de la prueba1 de capacidad
@@ -109,6 +149,9 @@ public class CapacidadAdaptacionManager : MonoBehaviour
     {
         resultadoPruebaCapacidad1 = "c";
         opcionElegidaResult1.text = resultadoPruebaCapacidad1;
+
+        //ponemos valor numerico segun el resultado
+        resultadoNumPruebaCapacidad1 = 3;
     }
 
     //para pasar a siguiente test
@@ -117,5 +160,102 @@ public class CapacidadAdaptacionManager : MonoBehaviour
         //pasariamos el resultadoPruebaCapacidad1 como string y argumento al metodo que lo subiese a la base de datos
 
         //pasa al siguiente test
+        test1Capacidad.SetActive(false);
+        test2Capacidad.SetActive(true);
     }
+
+    #endregion
+
+    #region methods Capacidad1
+
+    //metodo opcion A que abre panel opcion A de la pregunta 1
+    public void OpenOptionA2()
+    {
+        //quitamos botonContinue
+        botonContinue2.SetActive(false);
+        //cerramos menuPrincipal
+        menuPrincipal2.SetActive(false);
+        //abrimos panelesABC y opcion PanelA
+        panelesABC2.SetActive(true);
+        PanelA2.SetActive(true);
+    }
+
+    //metodo opcion B que abre panel opcion B de la pregunta 1
+    public void OpenOptionB2()
+    {
+        //quitamos botonContinue
+        botonContinue2.SetActive(false);
+        //cerramos menuPrincipal
+        menuPrincipal2.SetActive(false);
+        //abrimos panelesABC y opcion PanelA
+        panelesABC2.SetActive(true);
+        PanelB2.SetActive(true);
+    }
+
+    //metodo opcion B que abre panel opcion B de la pregunta 1
+    public void OpenOptionC2()
+    {
+        //quitamos botonContinue
+        botonContinue2.SetActive(false);
+        //cerramos menuPrincipal
+        menuPrincipal2.SetActive(false);
+        //abrimos panelesABC y opcion PanelA
+        panelesABC2.SetActive(true);
+        PanelC2.SetActive(true);
+    }
+
+    //metodo que cierra las opciones y permite volver a menuPrincipal
+    public void CerrarOpciones2()
+    {
+        //ponemos botonContinue
+        botonContinue2.SetActive(true);
+        //abrimos menuPrincipal
+        menuPrincipal2.SetActive(true);
+        //cerramos panelesABC, panelA, panelB y panelC
+        panelesABC2.SetActive(false);
+        PanelA2.SetActive(false);
+        PanelB2.SetActive(false);
+        PanelC2.SetActive(false);
+    }
+
+    //metodo que elige la opcion A de la prueba1 de capacidad
+    public void ChooseOptionACapacidad2()
+    {
+        resultadoPruebaCapacidad2 = "a";
+        opcionElegidaResult2.text = resultadoPruebaCapacidad2;
+
+        //ponemos valor numerico segun el resultado
+        resultadoNumPruebaCapacidad2 = 1;
+
+    }
+
+    //metodo que elige la opcion B de la prueba1 de capacidad
+    public void ChooseOptionBCapacidad2()
+    {
+        resultadoPruebaCapacidad2 = "b";
+        opcionElegidaResult2.text = resultadoPruebaCapacidad2;
+
+        //ponemos valor numerico segun el resultado
+        resultadoNumPruebaCapacidad2 = 2;
+    }
+
+    //metodo que elige la opcion C de la prueba1 de capacidad
+    public void ChooseOptionCCapacidad2()
+    {
+        resultadoPruebaCapacidad2 = "c";
+        opcionElegidaResult2.text = resultadoPruebaCapacidad2;
+
+        //ponemos valor numerico segun el resultado
+        resultadoNumPruebaCapacidad2 = 3;
+    }
+
+    //para pasar a siguiente test
+    public void ContinueNextQuestionCapacidad2()
+    {
+        //pasariamos el resultadoPruebaCapacidad1 como string y argumento al metodo que lo subiese a la base de datos
+
+        //pasa al siguiente test
+    }
+
+    #endregion
 }
