741e3f118a4b9f66e4de2934bd6e78cd58e532fc
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-09-20 15:57:22 +0200

    cambios text

--- Assets/escena login/loginBueno/scripts/UIManagerLogin.cs
+++ Assets/escena login/loginBueno/scripts/UIManagerLogin.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
