c361ea73bf432807ff3e41e5225f3e7bc5b8ca67
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-09-23 03:11:43 +0200

    ExplosionTierra hecho

---
+++ Assets/escenasNarrativas/IntroContexto/scripts/ExplosionTierra/ZoomOut.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class ZoomOut : MonoBehaviour
+{
+    public Camera camara; // Referencia a la cámara que deseas modificar
+    private float aumentoMaximo = 12.0f; // Tamaño máximo al que quieres aumentar la cámara
+    private float velocidadAumento = 0.5f; // Velocidad a la que aumenta el tamaño de la cámara
+
+    [SerializeField] private GameObject button;
+
+    private bool able = true;
+
+
+    private void Update()
+    {
+        if(able)
+        {
+            // Aumenta el tamaño gradualmente mientras no haya alcanzado el límite
+            if (camara.orthographicSize < aumentoMaximo)
+            {
+                float nuevoTamaño = Mathf.Lerp(camara.orthographicSize, aumentoMaximo, velocidadAumento * Time.deltaTime);
+                camara.orthographicSize = nuevoTamaño;
+            }
+            //si ha alcanzado el limite
+            if (camara.orthographicSize > 8)
+            {
+                //se activa boton
+                button.SetActive(true);
+                able = false;
+            }
+        }
+       
+    }
+
+    public void QuitarButton()
+    {
+        button.SetActive(false);
+    }
+}
