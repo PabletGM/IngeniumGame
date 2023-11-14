66b0dc6aca277408059b94fb129dd0c0bc4a9bce
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-11-14 13:01:57 +0100

    dialogue

--- Assets/tareaCaras/scripts/DialogueTrigger.cs
+++ Assets/tareaCaras/scripts/DialogueTrigger.cs
@@ -5,28 +5,15 @@ using Yarn.Unity;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    RaycastHit HitInfo;
-    public GameObject dialogue;
+    public GameObject lineView;
+    public GameObject optionsListView;
 
 
-    private void Update()
+   
+
+    public void ActivarDialogo()
     {
-        // Selecciona la capa que contiene tus sprites 2D
-        int layerMask = LayerMask.GetMask("objetoDialogue");
-
-        // Raycast desde la posición de la cámara en la dirección hacia adelante
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, Camera.main.transform.forward, 10, layerMask);
-
-        // Verifica si se ha golpeado un objeto
-        if (hit.collider != null)
-        {
-            // Verifica si se hizo clic derecho
-            if (Input.GetMouseButtonDown(1))
-            {
-                Debug.Log("Hey");
-                // Inicia el diálogo
-                dialogue.SetActive(true);
-            }
-        }
+        lineView.SetActive(true);
+        optionsListView.SetActive(true);
     }
 }
