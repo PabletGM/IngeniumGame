5391c7d9995a6f916ce1a855be1a5c087f6ce81b
Pablo <123249399+PabletGM@users.noreply.github.com>
2023-12-28 02:14:02 +0100

    changes

--- Assets/Secuencia3/scripts/3.1/AnimacionesManagerSecuencia3.cs
+++ Assets/Secuencia3/scripts/3.1/AnimacionesManagerSecuencia3.cs
@@ -14,6 +14,8 @@ public class AnimacionesManagerSecuencia3 : MonoBehaviour
     [SerializeField]
     public List<GameObject> listaGOEscena;
 
+    private int numeroCajasTiradasAnimacion = 4;
+
 
     private void Awake()
     {
@@ -33,25 +35,87 @@ public class AnimacionesManagerSecuencia3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        CancionSecuencia3Intro();
         ActivarAnimacionEscaleraObjetos();
+    }
+
+    private void CancionSecuencia3Intro()
+    {
+        //musica inicial intro
+        AudioManagerBengalas.instance.PlayMusic("introSecuencia3", 1f);
     }
 
     public void ActivarAnimacionEscaleraObjetos()
     {
+        //humo
+        HumoIntroVFX();
+        //rampa
+        Invoke("RampaIntroVFX", 0.2f);
+        //AudioManagerBengalas.instance.PlaySFX("rampa");
         //iniciamos primera animacion ESCALERA OBJETOS
         listaDeGameObjectsAnimacionOrden[0].GetComponent<AnimacionRampaObjetos>().ActivarAnimacionEscalera();
+        Invoke("StopRampaIntroVFX", 1f);
+    }
+
+    private void HumoIntroVFX()
+    {
+        AudioManagerBengalas.instance.PlaySFX("humo", 1f);
+    }
+
+    private void RampaIntroVFX()
+    {
+        AudioManagerBengalas.instance.PlaySFX("rampa", 0.5f);
+    }
+
+    private void StopRampaIntroVFX()
+    {
+        AudioManagerBengalas.instance.StopSFX();
+    }
+
+    private void StopRampaIntroVFX2()
+    {
+        AudioManagerBengalas.instance.StopSFX();
+        AudioManagerBengalas.instance.PlaySFX("hello", 1f);
     }
 
     public void ActivarAnimacionCajas()
     {
+        
         //iniciamos primera animacion ESCALERA OBJETOS
         listaDeGameObjectsAnimacionOrden[1].GetComponent<AnimacionCajasBajando>().ActivarAnimacionCajasBajando();
+        //sonido cajas
+        Invoke("CajasVFX", 0.8f);
+    }
+
+    private void CajasVFX()
+    {
+        AudioManagerBengalas.instance.PlaySFX("cajaSaltando", 1f);
+        Invoke("CajasVFX2", 0.8f);
+    }
+
+    private void CajasVFX2()
+    {
+        AudioManagerBengalas.instance.PlaySFX("cajaSaltando", 1f);
+        Invoke("CajasVFX3", 0.8f);
+    }
+
+    private void CajasVFX3()
+    {
+        AudioManagerBengalas.instance.PlaySFX("cajaSaltando", 1f);
+        Invoke("CajasVFX4", 0.8f);
+    }
+
+    private void CajasVFX4()
+    {
+        AudioManagerBengalas.instance.PlaySFX("cajaSaltando", 1f);
     }
 
     public void ActivarRampaPersonas()
     {
+        Invoke("RampaIntroVFX", 0.2f);
         //iniciamos primera animacion ESCALERA OBJETOS
         listaDeGameObjectsAnimacionOrden[2].GetComponent<AnimacionRampaPersonas>().ActivarRampaPersonas();
+        Invoke("StopRampaIntroVFX", 1f);
     }
 
     public void ActivarPersonasBajandoRampa()
@@ -70,6 +134,21 @@ public class AnimacionesManagerSecuencia3 : MonoBehaviour
         listaGOEscena[0].SetActive(true);
         //efecto multitud personas
         listaDeGameObjectsAnimacionOrden[4].GetComponent<AnimacionMultitudSaltando>().ActivarMultitudPersonas();
+        MultitudVFX();
+        //a los 3 o 4 segundos
+        Invoke("StopRampaIntroVFX2", 3.2f);
+        
+    }
+
+    private void HelloVFX()
+    {
+        Debug.Log("G");
+        
+    }
+
+    private void MultitudVFX()
+    {
+        AudioManagerBengalas.instance.PlaySFX("multitud", 1f);
     }
 
     public void ActivarJefeExploracionZoom()
@@ -77,6 +156,7 @@ public class AnimacionesManagerSecuencia3 : MonoBehaviour
         //activamos jefe de exploracion
         listaGOEscena[1].SetActive(true);
         listaDeGameObjectsAnimacionOrden[5].GetComponent<AnimacionJefeExploracion>().ActivarJefeExploracionn();
+        
     }
 
 
