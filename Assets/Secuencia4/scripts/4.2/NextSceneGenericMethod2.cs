52e99a8d0b966d7a4e700ae8b35335b525848734
Pablo <123249399+PabletGM@users.noreply.github.com>
2024-01-06 15:24:51 +0100

    4.1 y 4.2

--- Assets/Secuencia4/scripts/4.2/NextSceneGenericMethod2.cs
+++ Assets/Secuencia4/scripts/4.2/NextSceneGenericMethod2.cs
@@ -3,23 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-//metodo generico que al acabar una animacion pasa a la siguiente escena
-public class NextSceneGenericMethod : MonoBehaviour
+public class NextSceneGenericMethod2 : MonoBehaviour
 {
-
     [SerializeField]
     private float tiempoEsperaPasoEscena;
 
     [SerializeField]
     private string nombreCambioEscena;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Invoke("NextScene", tiempoEsperaPasoEscena);
-    }
-
-   public void NextScene()
+    
+    public void NextScene()
     {
         SceneManager.LoadScene(nombreCambioEscena);
     }
